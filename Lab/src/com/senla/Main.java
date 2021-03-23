29de4cfe36ec591148daf573515f08e1ba1c5b50
dilexet <63055058+dilexet@users.noreply.github.com>
2021-03-23 19:05:44 +0300

    start task_two

--- Lab/src/com/senla/Main.java
+++ Lab/src/com/senla/Main.java
@@ -1,23 +1,7 @@
 package com.senla;
 
-import com.senla.entity.Flower;
-import com.senla.entity.Rose;
-import com.senla.entity.Tulip;
-import com.senla.enums.Color;
-import com.senla.entity.FlowerShop;
-
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Flower> flowers = new ArrayList<>();
-        try {
-            flowers.add(new Rose(120, Color.RED));
-            flowers.add(new Tulip(220, Color.BLUE));
-        } catch (Exception ex) {
-            System.out.println(ex.toString());
-        }
-        var bouquet = FlowerShop.getFlowerShop().createBouquet(flowers);
-        System.out.println("Price bouquet = " + bouquet.getCost());
+
     }
 }
