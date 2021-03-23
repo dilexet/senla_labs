42a55010456076318d840c8de8cc6c20ac12f72d
dilexet <63055058+dilexet@users.noreply.github.com>
2021-03-23 18:57:47 +0300

    start task_two

--- Lab/src/com/senla/Main.java
+++ Lab/src/com/senla/Main.java
@@ -10,6 +10,14 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-       
+        ArrayList<Flower> flowers = new ArrayList<>();
+        try {
+            flowers.add(new Rose(120, Color.RED));
+            flowers.add(new Tulip(220, Color.BLUE));
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+        var bouquet = FlowerShop.getFlowerShop().createBouquet(flowers);
+        System.out.println("Price bouquet = " + bouquet.getCost());
     }
 }
---
+++ Lab/src/com/senla/entity/Rose.java
@@ -0,0 +1,9 @@
+package com.senla.entity;
+
+import com.senla.enums.Color;
+
+public class Rose extends Flower {
+    public Rose(double price, Color color) throws Exception {
+        super(price, "Rose", color);
+    }
+}
